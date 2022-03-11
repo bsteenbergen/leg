47ed63e3bd147022a47d0274fb73afd9db443496
Elena Martinez <41877941+elenasmartinez@users.noreply.github.com>
2022-03-11 14:13:40 -0800

    Update ast.test.js

--- test/ast.test.js
+++ test/ast.test.js
@@ -4,17 +4,27 @@ import ast from "../src/ast.js"
 
 // TODO: This test case needs a lot more work
 const source = `
-mumble("hi")
+num x = 2
+mumble("hi there!")
+task cube(y) yields int^3
+if z >= 0: 
+    yield z 
+else: 
+    yield -z
 `
 const expected = ` 
-1 | Program statements = [#2, ]
-2 | PrintStatement argument = 
+1 | Program statements = [#2, #3, #4, #7, #8]
+2 | VariableDeclaration modifier=(Sym, "num") variable=(Id, "x") initializer = (Int, "2")
+3 | PrintStatement argument=(Str, "hi there!")
+4 | FunctionDeclaration funName=(Id, "cube") params=[#5] returnExp = [#6]
+5 | Param name=(Id, "y"), paramCount=(Int, "1"), readOnly = ??????
+6 | 
+7 | IfLong tests = ???? consequent = [#8] alternate = [#9]
+8 | Loop 
 `
 
-// console.log(ast(source))
-
-// describe("The AST generator", () => {
-//   it("produces a correct AST", () => {
-//     assert.deepStrictEqual(util.format(ast(source)), expected)
-//   })
-// })
+describe("The AST generator", () => {
+    it("produces a correct AST", () => {
+        assert.deepStrictEqual(util.format(ast(source)), expected)
+    })
+})
