35194813066584cda8aa1da7b4d051a02abc9b70
Elena Martinez <41877941+elenasmartinez@users.noreply.github.com>
2022-03-14 00:32:04 -0700

    all functions covered except error functionality

--- test/ast.test.js
+++ test/ast.test.js
@@ -7,6 +7,7 @@ const source = `
 mumble("hi there!")
 num x = 2
 task cube(num y) yields int^3
+cube(12)
 if a >= 0  rem a / 2
 end
 task combineStrings(str str1, str str2) yields combinedString:
@@ -25,44 +26,45 @@ end
 ["1", "2", "3", "jump"]
 {"one" -> 1, "two" -> 2}
 `
-const expected = `   1 | Program statements=[#2,#3,#4,#7,#11,#17,#20,#23,#29,#35,#36]
+const expected = `   1 | Program statements=[#2,#3,#4,#7,#8,#12,#18,#21,#24,#30,#36,#37]
    2 | PrintStatement argument=(Str,""hi there!"")
    3 | VariableDeclaration modifier=(Sym,"num") variable='x' initializer=(Int,"2")
    4 | FunctionDeclaration funName=(Id,"cube") params=[#5] returnExp=[#6] body=[]
    5 | Param name=(Sym,"num") type='y'
    6 | BinaryExpression left=(Id,"int") op='^' right=(Int,"3")
-   7 | IfShort tests=#8 consequent=#9
-   8 | BinaryExpression left=(Id,"a") op='>=' right=(Int,"0")
-   9 | Suite statements=[#10]
-  10 | BinaryExpression left=(Id,"a") op='/' right=(Int,"2")
-  11 | FunctionDeclaration funName=(Id,"combineStrings") params=[#12,#13] returnExp=[(Id,"combinedString")] body=[#14]
-  12 | Param name=(Sym,"str") type='str1'
-  13 | Param name=(Sym,"str") type='str2'
-  14 | Suite statements=[#15]
-  15 | VariableDeclaration modifier=(Sym,"str") variable='combinedString' initializer=#16
-  16 | BinaryExpression left=(Id,"str1") op='+' right=(Id,"str2")
-  17 | FunctionDeclaration funName=(Id,"greeting") params=[#18] returnExp=[#19] body=[]
-  18 | Param name=(Sym,"str") type='name'
-  19 | BinaryExpression left=(Str,""hi"") op='+' right=(Id,"name")
-  20 | Dictionary expressions=[#21,#22]
-  21 | Binding key=(Int,"1") value=(Str,""one"")
-  22 | Binding key=(Int,"2") value=(Str,""two"")
-  23 | IfLong tests=#24 consequent=#25 alternate=#27
-  24 | BinaryExpression left=(Id,"x") op='>=' right=(Int,"0")
-  25 | Suite statements=[#26]
-  26 | VariableDeclaration modifier=(Sym,"bool") variable='greater' initializer=(Bool,"true")
-  27 | Suite statements=[#28]
-  28 | VariableDeclaration modifier=(Sym,"bool") variable='greater' initializer=(Bool,"false")
-  29 | IfShort tests=#30 consequent=#33
-  30 | BinaryExpression left=#31 op='&&' right=#32
-  31 | BinaryExpression left=(Id,"x") op='!' right=(Int,"1")
-  32 | BinaryExpression left=(Id,"x") op='!' right=(Int,"2")
-  33 | Suite statements=[#34]
-  34 | PrintStatement argument=(Str,""x is not 1 or 2"")
-  35 | List elements=[(Str,""1""),(Str,""2""),(Str,""3""),(Str,""jump"")]
-  36 | Dictionary expressions=[#37,#38]
-  37 | Binding key=(Str,""one"") value=(Int,"1")
-  38 | Binding key=(Str,""two"") value=(Int,"2")`
+   7 | Call callee=(Id,"cube") args=[(Int,"12")]
+   8 | IfShort tests=#9 consequent=#10
+   9 | BinaryExpression left=(Id,"a") op='>=' right=(Int,"0")
+  10 | Suite statements=[#11]
+  11 | BinaryExpression left=(Id,"a") op='/' right=(Int,"2")
+  12 | FunctionDeclaration funName=(Id,"combineStrings") params=[#13,#14] returnExp=[(Id,"combinedString")] body=[#15]
+  13 | Param name=(Sym,"str") type='str1'
+  14 | Param name=(Sym,"str") type='str2'
+  15 | Suite statements=[#16]
+  16 | VariableDeclaration modifier=(Sym,"str") variable='combinedString' initializer=#17
+  17 | BinaryExpression left=(Id,"str1") op='+' right=(Id,"str2")
+  18 | FunctionDeclaration funName=(Id,"greeting") params=[#19] returnExp=[#20] body=[]
+  19 | Param name=(Sym,"str") type='name'
+  20 | BinaryExpression left=(Str,""hi"") op='+' right=(Id,"name")
+  21 | Dictionary expressions=[#22,#23]
+  22 | Binding key=(Int,"1") value=(Str,""one"")
+  23 | Binding key=(Int,"2") value=(Str,""two"")
+  24 | IfLong tests=#25 consequent=#26 alternate=#28
+  25 | BinaryExpression left=(Id,"x") op='>=' right=(Int,"0")
+  26 | Suite statements=[#27]
+  27 | VariableDeclaration modifier=(Sym,"bool") variable='greater' initializer=(Bool,"true")
+  28 | Suite statements=[#29]
+  29 | VariableDeclaration modifier=(Sym,"bool") variable='greater' initializer=(Bool,"false")
+  30 | IfShort tests=#31 consequent=#34
+  31 | BinaryExpression left=#32 op='&&' right=#33
+  32 | BinaryExpression left=(Id,"x") op='!' right=(Int,"1")
+  33 | BinaryExpression left=(Id,"x") op='!' right=(Int,"2")
+  34 | Suite statements=[#35]
+  35 | PrintStatement argument=(Str,""x is not 1 or 2"")
+  36 | List elements=[(Str,""1""),(Str,""2""),(Str,""3""),(Str,""jump"")]
+  37 | Dictionary expressions=[#38,#39]
+  38 | Binding key=(Str,""one"") value=(Int,"1")
+  39 | Binding key=(Str,""two"") value=(Int,"2")`
 
 const mapSrc = `
 map <str, num> m = {"a" -> 1, "b" -> 2}
@@ -82,3 +84,9 @@ describe("The AST generator", () => {
     assert.deepStrictEqual(util.format(ast(mapSrc)), mapExpected)
   })
 })
+
+//describe("The error function", () => {
+  //it("throws an error when called", () => {
+   // assert.throws(error("Uh oh"))
+ // })
+//  })
